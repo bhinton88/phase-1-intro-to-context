cefc81884646a6c677a89f615800ffbccd647a0c
Bobby Hinton <bob.g.hint@gmail.com>
2022-06-19 19:16:08 -0500

    commit tree a664492be5f11b74d35825c0f2a3d03e868e95f5

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Your code here
+function createEmployeeRecord(employee) {
+  return {
+      firstName: employee[0],
+      familyName: employee[1],
+      title: employee[2],
+      payPerHour: employee[3],
+      timeInEvents: [],
+      timeOutEvents: []
+  }  
+}
+
+function createEmployeeRecords(array) {
+  return array.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent (employee, event) {
+   let [date, hour] = event.split(" ")
+   const timeInObj = {
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date
+   }
+   employee.timeInEvents.push(timeInObj)
+   return employee
+}
+
+function createTimeOutEvent (employee, event) {
+  let [date, hour] = event.split(" ")
+  const timeInObj = {
+     type: "TimeOut",
+     hour: parseInt(hour, 10),
+     date
+  }
+  employee.timeOutEvents.push(timeInObj)
+  return employee
+}
+
+function hoursWorkedOnDate (employee, date) {
+  const clockIn = employee.timeInEvents.find(event => event.date === date)
+  const clockOut = employee.timeOutEvents.find(event => event.date === date)
+  
+  return (clockOut.hour-clockIn.hour) / 100
+
+}
+
+function wagesEarnedOnDate(employee, date) {
+  return hoursWorkedOnDate(employee,date) * employee.payPerHour
+}
+
+function allWagesFor (employee) {
+// we want to pass an employee in, sum up the total hours worked by adding the hours
+// worked each day 
+return employee.timeInEvents.map(data => wagesEarnedOnDate(employee, data.date)).reduce((previousValue, initialValue) => initialValue + previousValue)
+}
+
+function calculatePayroll(employees) {
+  return employees.map(employee => employee.timeInEvents.map(data => wagesEarnedOnDate(employee, data.date)).reduce((previousValue, initialValue) => initialValue + previousValue)).reduce((previous,current) => previous + current)
+}
